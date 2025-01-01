ecec9f84a998f241764a67d378858a16aba82ead
Darkmysth <pelletier.thom@gmail.com>
2025-01-01 20:16:53 +0100

    Initial Commit

---
+++ VinSurVinTests/VinSurVinTests.swift
@@ -0,0 +1,16 @@
+//
+//  VinSurVinTests.swift
+//  VinSurVinTests
+//
+//  Created by Thomas Pelletier on 01/01/2025.
+//
+
+import Testing
+
+struct VinSurVinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
